e2080017937aede2d2cf49c73bc40b8cbac55468
jkroloand2312 <juniormn123@gmail.com>
2019-12-03 10:43:47 +0700

    sdfsdf

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        ijioiojoiio
+        <img src={logo} className="App-logo" alt="logo" /
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
